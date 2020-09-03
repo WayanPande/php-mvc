42e09d4e50f520ecfdb108818d5739911afbca7c
WayanPande <yande554@gmail.com>
2020-09-03 21:13:33 +0800

    Menambah fungsi controller

---
+++ app/controllers/About.php
@@ -0,0 +1,14 @@
+<?php
+
+class About
+{
+
+    public function index($nama = "Lojik", $pekerjaan = "wadidwa")
+    {
+        echo "Hallo nama saya $nama, saya seorang $pekerjaan";
+    }
+    public function page()
+    {
+        echo 'About/page';
+    }
+}

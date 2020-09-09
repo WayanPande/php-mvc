260e88013f5eb80c77dea6b0499db40ba8757beb
WayanPande <yande554@gmail.com>
2020-09-09 21:09:11 +0800

    Menambahkan if isset untuk controller

--- app/core/App.php
+++ app/core/App.php
@@ -11,9 +11,11 @@ class App
     $url = $this->parseURL();
 
     // controller
-    if (file_exists('../app/controllers/' . $url[0] . '.php')) {
-      $this->controller = $url[0];
-      unset($url[0]);
+    if (isset($url[0])) {
+      if (file_exists('../app/controllers/' . $url[0] . '.php')) {
+        $this->controller = $url[0];
+        unset($url[0]);
+      }
     }
 
     require_once '../app/controllers/' . $this->controller . '.php';
@@ -39,8 +41,8 @@ class App
 
   public function parseURL()
   {
-    if (isset($_GET["url"])) {
-      $url = rtrim($_GET["url"], '/');
+    if (isset($_GET['url'])) {
+      $url = rtrim($_GET['url'], '/');
       $url = filter_var($url, FILTER_SANITIZE_URL);
       $url = explode('/', $url);
       return $url;
